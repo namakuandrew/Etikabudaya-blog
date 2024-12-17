05e5a2f1a7e4c0bd086964e4652f347f220a912d
namakuandrew <thesky11.lay@gmail.com>
2024-12-17 14:28:27 +0700

    feat/ add team name

--- Etikabudaya-blog-master/animation.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  const headerText = document.getElementById("Header-text");
-
-  headerText.style.animation = "fadeSlideIn 2s forwards";
-};
-
-const headerBackground = document.querySelector(".Header-background");
-
-headerBackground.classList.add("animate-slide");
